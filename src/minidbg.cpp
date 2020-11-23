312e8672314f5ac334f5e6fc09bf64a76315ba2d
Fereshteh FeizabadiFarahani <34858445+FereshtehFeiz@users.noreply.github.com>
2020-11-23 22:11:06 +0100

    comments added

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -28,6 +28,13 @@ bool is_prefix(const std::string& s, const std::string& of) {
     return std::equal(s.begin(), s.end(), of.begin());
 }
 
+//Handling input
+
+//Our commands will follow a similar format to gdb and lldb. 
+//To continue the program, a user will type continue or cont or even just c. 
+//If they want to set a breakpoint on an address, they’ll write break 0xDEADBEEF, where 0xDEADBEEF is the desired address in hexadecimal format.
+// Let’s add support for these commands.
+
 void debugger::handle_command(const std::string& line) {
     auto args = split(line,' ');
     auto command = args[0];
@@ -39,6 +46,8 @@ void debugger::handle_command(const std::string& line) {
         std::cerr << "Unknown command\n";
     }
 }
+
+//In our run function, we need to wait until the child process has finished launching, then keep on getting input from linenoise until we get an EOF (ctrl+d)
 
 void debugger::run() {
     int wait_status;
@@ -54,6 +63,7 @@ void debugger::run() {
 }
 
 void debugger::continue_execution() {
+    //ptrace allows us to observe and control the execution of another process by reading registers, reading memory, single stepping and more
     ptrace(PTRACE_CONT, m_pid, nullptr, nullptr);
 
     int wait_status;
@@ -62,13 +72,16 @@ void debugger::continue_execution() {
 }
 
 void execute_debugee (const std::string& prog_name) {
+    //running child process 
     if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
         std::cerr << "Error in ptrace\n";
         return;
     }
+    //We execute the given program, passing the name of it as a command-line argument and a nullptr to terminate the list
     execl(prog_name.c_str(), prog_name.c_str(), nullptr);
 }
 
+//main class
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cerr << "Program name not specified";
@@ -86,8 +99,10 @@ int main(int argc, char* argv[]) {
     else if (pid >= 1)  {
         //parent
         std::cout << "Started debugging process " << pid << '\n';
+        //debugger for interacting with child process
         debugger dbg{prog, pid};
         dbg.run();
     }
 }
+//In our run function, we need to wait until the child process has finished launching, then keep on getting input from linenoise until we get an EOF (ctrl+d)
 
